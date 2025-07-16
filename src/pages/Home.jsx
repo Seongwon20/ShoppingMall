31fa11dd67c071ecd2d6307edd3280ebcf47d453
Seongwon20 <98306482+Seongwon20@users.noreply.github.com>
2025-07-16 21:06:09 +0900

    feat: Home 까지 완성

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Hero from '../components/Hero.jsx'
+import LatestCollection from '../components/LatestCollection.jsx'
+
+const Home = () => {
+  return (
+    <div>
+      <Hero />
+      <LatestCollection />
+    </div>
+  )
+}
+
+export default Home
